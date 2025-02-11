5bf81c2774375931de67893b665057717acc6e4f
Nenzy10 <111493268+Nenzy10@users.noreply.github.com>
2025-02-11 11:10:43 -0500

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-// Serve static files from the public directory
+// Serve static files from the public directory (ensure you have a "public" folder)
 app.use(express.static('public'));
 
 // Serve the DApp HTML page when accessing the root URL
