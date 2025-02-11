ad24036fe1ad8e646756ea577f247e49331aa14c
Nenzy10 <111493268+Nenzy10@users.noreply.github.com>
2025-02-11 12:25:04 -0500

    Update server.js

--- server.js
+++ server.js
@@ -2,16 +2,15 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-// Serve static files from the public directory (ensure you have a "public" folder)
-app.use(express.static('public'));
+// Serve static files from the same directory as index.html
+app.use(express.static(path.join(__dirname)));
 
-// Serve the DApp HTML page when accessing the root URL
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running at http://localhost:${port}`);
 });
